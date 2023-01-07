3659614c67b1e82592866447984a16fb3449ee8c
Janet Blackquill <uhhadd@gmail.com>
2023-01-07 13:31:41 -0500

    more lotto

--- Sources/App/Controllers/LottoController.swift
+++ Sources/App/Controllers/LottoController.swift
@@ -5,8 +5,40 @@ struct LottoController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let group = routes.grouped("lotto")
         group.post("create", use: createLotto)
+        group.get("my", use: myLottos)
         group.get(":lotto", use: getInformation)
         group.post(":lotto", "buy-ticket", use: buyTicket)
+        group.post(":lotto", "roll-winner", use: rollWinner)
+    }
+    struct MyLottosResponse: Content {
+        let lottos: Page<Lotto>
+    }
+    func myLottos(on req: Request) async throws -> MyLottosResponse {
+        let user = try req.auth.require(User.self)
+        let lottos = try await Lotto.query(on: req.db).filter(\.$creator.$id == user.customer.id!).paginate(for: req)
+        return MyLottosResponse(lottos: lottos)
+    }
+    struct RollWinner: Content {
+        let who: Customer
+    }
+    func rollWinner(on req: Request) async throws -> RollWinner {
+        let user = try req.auth.require(User.self)
+        let slug = req.parameters.get(":lotto")!
+        guard let lotto = try await Lotto.query(on: req.db).filter(\.$slug == slug).first() else {
+            throw Abort(.notFound)
+        }
+        guard user.customer.id! == lotto.$creator.id else {
+            throw Abort(.forbidden)
+        }
+        let tickets = try await lotto.$involved.query(on: req.db).all()
+        guard let winning = tickets.randomElement() else {
+            throw Abort(.forbidden) // TODO: better error code
+        }
+        lotto.$winner.id = winning.$buyer.id
+        try await winning.$buyer.load(on: req.db)
+        try await winning.buyer.$user.load(on: req.db)
+        try await lotto.save(on: req.db)
+        return RollWinner(who: winning.buyer)
     }
     struct CreateLottoResponse: Content {
         let lotto: Lotto
@@ -47,7 +79,8 @@ struct LottoController: RouteCollection {
             throw Abort(.notFound)
         }
         if user.customer.id! != lotto.$creator.id {
-            return GetInformationResponse(lotto: lotto, tickets: [])
+            let tickets = try await lotto.$involved.query(on: req.db).filter(\.$buyer.$id == user.customer.id!).all()
+            return GetInformationResponse(lotto: lotto, tickets: tickets)
         }
         let tickets = try await lotto.$involved.query(on: req.db).all()
         return GetInformationResponse(lotto: lotto, tickets: tickets)
